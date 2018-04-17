ffe8f3480a6e5c5459b0076af445bc7cc75cec82
sairoutine <sairoutine@gmail.com>
2018-04-18 00:56:51 +0900

    update: part1 section 2

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 );
 
+type InputBuffer struct {
+	Buffer string
+	BufferLength int
+	InputLength int
+}
+
+
 func main() {
 	input_buffer := new_input_buffer();
 
@@ -11,23 +18,27 @@ func main() {
 		print_prompt();
 		read_input(input_buffer);
 
-		if input_buffer.buffer == ".exit" {
+		if input_buffer.Buffer == ".exit" {
 			os.Exit(0);
 		} else {
-			fmt.Printf("Unrecognized command '%s'.\n", input_buffer.buffer);
+			fmt.Printf("Unrecognized command '%s'.\n", input_buffer.Buffer);
 		}
 	}
 }
 
 
-func new_input_buffer() int {
+func new_input_buffer() InputBuffer {
+	input_buffer := InputBuffer{};
+	input_buffer.Buffer = "";
+	input_buffer.BufferLength = 0;
+	input_buffer.InputLength = 0;
 
-	return 0;
+	return input_buffer;
 }
 
 func print_prompt() {
 }
-func read_input(input_buffer int) {
+func read_input(input_buffer InputBuffer) {
 }
 
 
